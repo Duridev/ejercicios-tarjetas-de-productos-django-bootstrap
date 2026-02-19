f81d8cd1f1714338ed036dc1de5e9f07b78bfbcf
duridev <duridev@outlook.com>
2026-02-18 23:24:43 -0300

    initialize tarjetas app with basic structure and templates

---
+++ tarjetas/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    data = [
+        {
+            'nombre': '',
+            'precio': '',
+            'categoria': '',
+            'imagen': '',
+        },
+        {
+            'nombre': '',
+            'precio': '',
+            'categoria': '',
+            'imagen': '',
+        },
+                {
+            'nombre': '',
+            'precio': '',
+            'categoria': '',
+            'imagen': '',
+        },
+                        {
+            'nombre': '',
+            'precio': '',
+            'categoria': '',
+            'imagen': '',
+        },
+                                {
+            'nombre': '',
+            'precio': '',
+            'categoria': '',
+            'imagen': '',
+        },
+                                        {
+            'nombre': '',
+            'precio': '',
+            'categoria': '',
+            'imagen': '',
+        },
+    ]
